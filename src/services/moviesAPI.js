c379b8c082f059ef7321c8536f26caeede058f64
Dafarlog <Dafarlog@gmail.com>
2022-10-21 15:50:40 +0300

    add fetchMoviesById

--- src/services/moviesAPI.js
+++ src/services/moviesAPI.js
@@ -4,6 +4,7 @@ export class moviesApi {
   static baseUrl = 'https://api.themoviedb.org/3';
   static trendingEndPoint = '/trending/movie/day';
   static searchEndPoint = '/search/movie';
+  static movieEndPoint = '/movie';
   static async searchMovies(query) {
     if (!query) return undefined;
     const { data } = await axios.get(`${this.baseUrl}${this.searchEndPoint}`, {
@@ -17,6 +18,17 @@ export class moviesApi {
   static async fetchTrendingMoviesToday() {
     const { data } = await axios.get(
       `${this.baseUrl}${this.trendingEndPoint}`,
+      {
+        params: {
+          api_key: 'fd0a7baec7e1722e7549dfbeea65227f',
+        },
+      }
+    );
+    return data;
+  }
+  static async fetchMoviesById(id) {
+    const { data } = await axios.get(
+      `${this.baseUrl}${this.movieEndPoint}/${id}`,
       {
         params: {
           api_key: 'fd0a7baec7e1722e7549dfbeea65227f',
