23a68a17fdbba1e4cbcb66cfaddee95612679391
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2022-10-23 14:15:05 +0300

    add search hook

--- src/hooks/useSearcheMovies.js
+++ src/hooks/useSearcheMovies.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import { moviesApi } from 'services/moviesAPI';
 
-export const useFetchTrendingMovie = () => {
+export const useSearcheMovies = q => {
   const [movies, setMovies] = useState();
+  //   const [query] = useState(q);
 
   useEffect(() => {
-    moviesApi.fetchTrendingMoviesToday().then(setMovies);
+    moviesApi.searchMovies(q).then(setMovies);
   }, []);
+
   return movies;
 };
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,32 +1,34 @@
 import { MoviesList } from 'components/MoviesList/MoviesList';
 import { Searchbar } from 'components/Searchebar/Searchbar';
+import { useSearcheMovies } from 'hooks/useSearcheMovies';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { moviesApi } from 'services/moviesAPI';
 
 export default function MoviesPage() {
-  const [movies, setMovies] = useState([]);
   const [searchParams] = useSearchParams();
-  useEffect(() => {
-    console.log('q');
-    fetchMovies();
-  }, []);
+  const movies = useSearcheMovies(searchParams.get('query'));
+  console.log(movies);
+  // useEffect(() => {
+  //   console.log('q');
+  //   fetchMovies();
+  // }, []);
 
-  const fetchMovies = async () => {
-    try {
-      const resp = await moviesApi.searchMovies(searchParams.get('query'));
-      if (!resp) return;
-      const { results } = resp;
-      setMovies(results);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  // const fetchMovies = async () => {
+  //   try {
+  //     const resp = await moviesApi.searchMovies(searchParams.get('query'));
+  //     if (!resp) return;
+  //     const { results } = resp;
+  //     setMovies(results);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
   return (
     <>
       <Searchbar />
-      <MoviesList movies={movies} />
+      {movies && <MoviesList movies={movies.results} />}
     </>
   );
 }
